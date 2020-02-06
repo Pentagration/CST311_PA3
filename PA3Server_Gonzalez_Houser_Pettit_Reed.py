3f3fcc2c7eb53aecda84dc5591239e374b21e260
Adam Houser <aphouser@gmail.com>
2020-02-06 10:53:04 -0800

    Initial setup with default code

---
+++ PA3Server_Gonzalez_Houser_Pettit_Reed.py
@@ -0,0 +1,20 @@
+# PA3Server_Gonzalez_Houser_Pettit_Reed.py
+# Marcus Gonzalez, Adam Houser, Jason Pettit, Colin Reed
+
+# placeholder code below
+from socket import *
+serverPort = 12000
+
+# Create a TCP socket
+# Notice the use of SOCK_STREAM for TCP packets
+serverSocket = socket(AF_INET,SOCK_STREAM)
+
+# Assign IP address and port number to socket
+serverSocket.bind(('',serverPort)) serverSocket.listen(1)
+print ('The server is ready to receive')
+while True:
+    connectionSocket, addr = serverSocket.accept()
+    sentence = connectionSocket.recv(1024).decode()
+    capitalizedSentence = sentence.upper()
+    connectionSocket.send(capitalizedSentence.encode())
+    connectionSocket.close()
