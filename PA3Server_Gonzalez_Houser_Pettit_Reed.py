3189c3ecff1becd7e84ba9ed51547802932add2e
jasonpettit <46112312+jasonpettit@users.noreply.github.com>
2020-02-15 15:34:20 -0800

    Update PA3Server_Gonzalez_Houser_Pettit_Reed.py

--- PA3Server_Gonzalez_Houser_Pettit_Reed.py
+++ PA3Server_Gonzalez_Houser_Pettit_Reed.py
@@ -1,31 +1,47 @@
 # PA3Server_Gonzalez_Houser_Pettit_Reed.py
 # Marcus Gonzalez, Adam Houser, Jason Pettit, Colin Reed
 
-# placeholder code below
 from socket import *
 from socket import socket
-from _thread import *
 import threading
 
+# Thread function
+def thread_accept(index, message, lock, sockets):
+    connectionSocket, addr = serverSocket.accept()  # accept a client connection
+    sockets.append(connectionSocket)                # store client sockets
+    lock.acquire()                                  # acquire a lock to prevent simultaneous writes to message
+    sentence = connectionSocket.recv(1024).decode() # sentence = Client X: Alice or Client Y: Bob
+    message.append(sentence)                        # keep track of which sentence arrived first
+    lock.release()                                  # release the lock
+
+# START SERVER SETUP
+#Assign TCP port number
 serverPort = 12000
 
 # Create a TCP socket
 # Notice the use of SOCK_STREAM for TCP packets
 serverSocket = socket(AF_INET,SOCK_STREAM)
 
-# Assign IP address and port number to socket
-serverSocket.bind(('',serverPort)) 
+#Assign IP address and port number to socket
+serverSocket.bind(('',serverPort))
 serverSocket.listen(1)
-print ('The server is ready to receive')
+print('The server is ready to receive')
+#END SERVER SETUP
 
 while True:
-    connectionSocket, addr = serverSocket.accept()
-    sentence1 = connectionSocket.recv(1024).decode()
-    #sentence2 = connectionSocket.recv(1024).decode()
-    #capitalizedSentence = sentence1 + 'received before' + sentence2
-    capitalizedSentence = sentence1 + ' received before '
+    sockets = list()        # keep track of the sockets to send our final message to
+    threads = list()        # keep track of threads
+    message = list()        # keep track of which message arrived first
+    lock = threading.Lock() # lock to keep multiple clients from writing simultaneously
 
-    connectionSocket.send(capitalizedSentence.encode())
-    connectionSocket.close()
+    # Start two threads
+    for index in range(2):
+        x = threading.Thread(target=thread_accept, args=(index,message,lock,sockets))
+        threads.append(x)
+        x.start()
+
+    # Join two threads
+    for index, thread in enumerate(threads):
+        thread.join()
 
     print('Sent acknowledgement to both X and Y')
